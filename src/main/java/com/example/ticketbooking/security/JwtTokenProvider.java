9f5fe7bcb1867e74a54c63647c4f33211846dbeb
Alogo Situmorang <aekmohop@gmail.com>
2025-06-28 21:20:23 +0700

    Update JwtTokenProvider.java

--- src/main/java/com/example/ticketbooking/security/JwtTokenProvider.java
+++ src/main/java/com/example/ticketbooking/security/JwtTokenProvider.java
@@ -20,7 +20,7 @@ public class JwtTokenProvider {
     @Value("${app.jwt.secret:}")
     private String jwtSecret;
 
-    @Value("${app.jwt.expiration:86400000}") // 24 hours in milliseconds
+    @Value("${app.jwt.expiration:}") 
     private long jwtExpirationMs;
 
     private SecretKey getSigningKey() {
